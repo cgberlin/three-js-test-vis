730b1dbafa3f009cf8a15d26fb0330bdddc1ac14
cgberlin <codyberlin@gmail.com>
2017-11-18 00:48:34 -0800

    added planets, audio controls, moving stars, pizzaz, its all in here folks

--- js/main.js
+++ js/main.js
@@ -65,6 +65,16 @@ $(function() {
     }
   })
 
+  var bMobile =   // will be true if running on a mobile device
+  navigator.userAgent.indexOf( "Mobile" ) !== -1 ||
+  navigator.userAgent.indexOf( "iPhone" ) !== -1 ||
+  navigator.userAgent.indexOf( "Android" ) !== -1 ||
+  navigator.userAgent.indexOf( "Windows Phone" ) !== -1 ;
+
+  if (!bMobile) {
+    $('#mobileWarning').css('display', 'none')
+  }
+
 
 
 
